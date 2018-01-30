3575112a5e78af81e2bf6d8dbf2e41f8a0b1321f
mlampert84 <marcus.lampert@gmail.com>
2018-01-30 12:33:33 +0100

    Initial Commit of App

---
+++ index.js
@@ -0,0 +1,40 @@
+
+var https = require('https')
+var moment = require('moment')
+const PORT = process.env.PORT || 5000
+
+var server = https.createServer()
+
+server.on('request',(req,res)=>{
+    
+  //cut off the initial "/" of the get request
+  var dateReq = req.url.slice(1)
+  
+  //reinserting white space after the get request converts it to %20
+  var dateToParse = dateReq.replace(/%20/g,' ')
+
+
+  var parsedDate = moment(dateToParse,["X",'MMMM D, YYYY'])   
+  
+  var displayDate = new Object()
+
+  if(parsedDate.isValid()){
+     displayDate.unix = +parsedDate.format("X")
+     displayDate.natural = parsedDate.format("MMMM D, YYYY") 
+  }
+  else{
+    displayDate.unix = null
+    displayDate.natural = null 
+  }
+//    res.end(dateToParse)
+  res.end(JSON.stringify(displayDate))
+})
+
+function parseDate(dateString){
+  
+
+}  
+
+
+
+server.listen(PORT)
